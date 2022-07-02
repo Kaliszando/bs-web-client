c59338ce57b4d413b7514b780212ad8d55d81dc5
Kaliszando <kaliszadam99@gmail.com>
2022-07-02 12:31:42 +0200

    add style files, based on 7-1 pattern

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'bs-root',
   templateUrl: './app.component.html',
+  styleUrls: ['app.component.scss']
 })
 export class AppComponent {
   title = 'bs-web-client';
