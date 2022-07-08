b800dfd847d3194dd1cf94d0226a56fba45ea038
Kaliszando <kaliszadam99@gmail.com>
2022-07-08 13:55:29 +0200

    - added logo - configured favicon

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 
-
 @NgModule({
-  declarations: [],
+  declarations: [
+  ],
   imports: [
     CommonModule
   ]
