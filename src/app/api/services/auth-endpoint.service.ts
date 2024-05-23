0e7ddcbb208d3ac103ab54b0f9413fdcc717a7eb
Adam Kalisz <kaliszadam99@gmail.com>
2024-05-23 21:32:24 +0200

    BS-85 sign-out endpoint integration

--- src/app/api/services/auth-endpoint.service.ts
+++ src/app/api/services/auth-endpoint.service.ts
@@ -73,6 +73,53 @@ export class AuthEndpointService extends BaseService {
   }
 
   /**
+   * Path part for operation signOut
+   */
+  static readonly SignOutPath = '/auth/sign-out';
+
+  /**
+   * User logout endpoint
+   *
+   * This method provides access to the full `HttpResponse`, allowing access to response headers.
+   * To access only the response body, use `signOut()` instead.
+   *
+   * This method doesn't expect any request body.
+   */
+  signOut$Response(params?: {
+  }): Observable<StrictHttpResponse<void>> {
+
+    const rb = new RequestBuilder(this.rootUrl, AuthEndpointService.SignOutPath, 'post');
+    if (params) {
+    }
+
+    return this.http.request(rb.build({
+      responseType: 'text',
+      accept: '*/*'
+    })).pipe(
+      filter((r: any) => r instanceof HttpResponse),
+      map((r: HttpResponse<any>) => {
+        return (r as HttpResponse<any>).clone({ body: undefined }) as StrictHttpResponse<void>;
+      })
+    );
+  }
+
+  /**
+   * User logout endpoint
+   *
+   * This method provides access to only to the response body.
+   * To access the full response (for headers, for example), `signOut$Response()` instead.
+   *
+   * This method doesn't expect any request body.
+   */
+  signOut(params?: {
+  }): Observable<void> {
+
+    return this.signOut$Response(params).pipe(
+      map((r: StrictHttpResponse<void>) => r.body as void)
+    );
+  }
+
+  /**
    * Path part for operation ping
    */
   static readonly PingPath = '/auth/ping';
