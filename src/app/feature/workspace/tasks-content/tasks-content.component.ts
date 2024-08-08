54c89f51db9b82edf0a6bcfcbb8aeadd2a6e5205
Adam Kalisz <kaliszadam99@gmail.com>
2024-08-08 20:05:16 +0200

    BS-10 WEB fixed displaying details tab on leave

--- src/app/feature/workspace/tasks-content/tasks-content.component.ts
+++ src/app/feature/workspace/tasks-content/tasks-content.component.ts
@@ -1,22 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from "@angular/router";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'bs-tasks-content',
-  templateUrl: './tasks-content.component.html',
-  styleUrls: ['./tasks-content.component.scss']
+  templateUrl: 'tasks-content.component.html',
+  styleUrls: ['tasks-content.component.scss'],
 })
-export class TasksContentComponent implements OnInit{
-
+export class TasksContentComponent implements OnInit {
   routeId = '';
 
   constructor(public router: Router) {}
 
   ngOnInit(): void {
-    this.routeId = this.router.url.substring(20)
+    this.routeId = this.router.url.substring(20);
   }
 
   onNavChange() {
-    this.routeId = this.router.url.substring(20)
+    this.routeId = this.router.url.substring(20);
   }
 }
